650dbc0ad61276cf8ed7abc9f324a8cf1aeef753
KAVI <kavindu123450@gmail.com>
2022-12-28 22:45:34 +0530

    timer and over

--- index.js
+++ index.js
@@ -123,6 +123,33 @@ function rectangular_collision( {rec1, rec2} ){
     )
 }
 
+function determineWinner({player,enemy,timerId}){
+    clearTimeout(timerId)
+    document.querySelector('#display_text').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#display_text').innerHTML = 'Tie'
+    } else if (player.health > enemy.health) {
+        document.querySelector('#display_text').innerHTML = 'Player 1 Wins'
+    } else if (enemy.health > player.health) {
+        document.querySelector('#display_text').innerHTML = 'Player 2 Wins'
+    }
+}
+
+let timer = 10
+let timerId
+function decreaseTimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer,1000)
+        timer--
+        document.querySelector('#time').innerHTML = timer
+    }
+//end game timeout
+    if (timer===0) {
+        determineWinner({player,enemy,timerId})
+    }
+}
+decreaseTimer()
+
 function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
@@ -165,6 +192,11 @@ function animate() {
         enemy.isAttacking = false;
         player.health -= 20;
         document.querySelector('#player_health').style.width = player.health + '%'
+    }
+
+    // end game over health
+    if (player.health <= 0 || enemy.health <= 0){
+        determineWinner({player,enemy,timerId})
     }
 }
 
